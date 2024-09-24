cfdc81155532927992561fc370cf0a5513eb2721
GPDBalde <gerard_balde@yahoo.com>
2024-09-24 21:28:21 +0800

    Modified guidelins page

--- application/modules/client/views/guidelines.php
+++ application/modules/client/views/guidelines.php
@@ -1,46 +1,14 @@
 <?php error_reporting(0);?>
-<div class="container-fluid" style="margin-top:70px; background-color:#F3F4F6">
-
-	<div class="row">
-		<div class="col">
-			<img src="<?php echo base_url("assets/images/banner2024.png"); ?>" class="img-fluid w-100"
-				alt="Responsive image">
+<div class="container-fluid">
+    <div class="row pt-3">
+        <div class="col col-3 p-3">
+            <button class="btn btn-link main-link">View all articles</button>
 		</div>
-	</div>
-	<br />
-	<div class="row">
-		<!-- SIDE NAVIGATION -->
-		<div class="col-2 mb-5">
-			<div class="list-group sticky" id="list_group_menu">
-				<li class="list-group-item font-weight-bold bg-light"><span class="oi oi-book"></span> JOURNALS</li>
-				<?php if ($journals != null) {
-				$c = 0;foreach ($journals as $row): ?>
-				<?php $c++;?>
-				<?php echo '<a href="'.base_url('/client/ejournal/get_issues/'.$row->jor_volume.'').'" class="list-group-item  list-group-item-action">Volume ' . $row->jor_volume . ', ' . $row->jor_year . '<span class="oi oi-chevron-right float-right mt-1" style="font-size:10px"></span></a>'; ?>
-				<?php endforeach;}?>
-			</div>
-		</div>
-		<div class="col-10">
-			<div class="tab-content" id="nav-tabContent">
-
-				<!-- GUIDELINES -->
-				<div class="tab-pane fade show active " id="home" role="tabpanel" aria-labelledby="home">
-					<div class="jumbotron shadow p-5 mb-5 bg-white rounded">
-						<div class="row">
-							<div class="col">
-								<div class="btn-group" role="group" aria-label="Basic example">
-								<a type="button" class="btn btn-dark mr-1 text-white disabled">ARTICLES:</a>
-									<a href="<?php echo base_url('/client/ejournal/get_index');?>" type="button" class="btn btn-outline-primary mr-1">INDEX</a>
-									 <?php foreach (range('A', 'Z') as $char) {
-										echo '<a href="'.base_url('/client/ejournal/get_index/'.$char.'').'" type="button" class="btn btn-outline-primary mr-1">'. $char .'</a>';
-										}
-									 ?>
-								</div>
-							</div>
-						</div>
+		<div class="col col-7 p-3">
+					<div class="shadow p-5 mb-5 bg-white rounded">
 						<div class="row mt-5">
 							<div class="col">
-								 <h2>SUBMISSION OF MANUSCRIPTS</h2>
+								 <h3>SUBMISSION OF MANUSCRIPTS</h3>
 									<ol>
 										<li>Only NRCP members can submit manuscript.</li>
 										<li>
@@ -63,7 +31,7 @@
 										</li>
 									</ol>
 
-									<h2>GENERAL INSTRUCTIONS TO AUTHORS</h2>
+									<h3>GENERAL INSTRUCTIONS TO AUTHORS</h3>
 									<ol>
 										<li>
 										Submit manuscripts in word and pdf format as required by the online form.
@@ -206,8 +174,9 @@
 								target="_blank">Creative Commons Attribution 4.0 International License</a>.
 						</div>
 					</div>
-				</div>
-			</div>
+		</div>
+		<div class="col col-2 p-3">
+			<?php $this->load->view('common/side_panel');?>
 		</div>
 	</div>
     </div>
