a4a2fc0db96acf0e11d5b9a4014b8f500b70f039
GPDBalde <gerard_balde@yahoo.com>
2024-10-30 19:00:09 +0800

    Modified side panel international editorial board link and no data availbale in annual journal metrcis

--- application/modules/client/views/common/side_panel.php
+++ application/modules/client/views/common/side_panel.php
@@ -3,7 +3,7 @@
                     <h6 class="card-title fw-bold d-block">Journal Information</h6>
                     <a href="<?php echo base_url('/client/ejournal/editorial');?>"
                         class="text-dark text-decoration-underline">Editorial Board</a>
-                    <a href="#" class="text-dark text-decoration-underline">International Editorial Board</a>
+                    <a href="#" class="text-dark text-decoration-underline" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Ongoing Development">International Editorial Board</a>
                 </div>
             </div>
 
@@ -11,7 +11,9 @@
                 <div class="card-body">
                     <h6 class="card-title fw-bold">Annual Journal Metrics</h6>
                     <h6 class="card-title fw-bold mt-3 mb-0">Citation Impact <?php echo date('Y');?></h6>
+                    -
                     <h6 class="card-title fw-bold mt-2 mb-0">Speed <?php echo date('Y');?></h6>
+                    -
                     <h6 class="card-title fw-bold mt-2 mb-0">Usage <?php echo date('Y');?></h6>
                     <div>Downloads: <?=number_format($downloads, 0, '', ',')?></div>
                     <div>Altmetric mentions: <?=number_format($citations, 0, '', ',')?></div>
